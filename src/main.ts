ecf8aab2a1c98250036cc04ef66adbd3f1892d54
Dekadence <deathfurious@hotmail.fr>
2023-04-29 03:37:53 +0200

    Add Bootstrap
    Changes:
    - Add new package (bootstrap-vue) to package.json
    - Update main.ts to import Bootstrap CSS and JS

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,12 @@ import { createApp } from 'vue'
 
 import router from './router'
 
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.js';
+
 import './style.css'
 import App from './App.vue'
-  
-createApp(App).use(router).mount('#app')
+
+createApp(App)
+    .use(router)
+    .mount('#app')
