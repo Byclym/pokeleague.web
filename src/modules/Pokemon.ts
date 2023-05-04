3b199aa8af9e4f61b904ed99550df99ba8420672
Dekadence <deathfurious@hotmail.fr>
2023-05-04 03:25:26 +0200

    Refactor Pokemon class
    Changes:
    
    Modify Pokemon class to directly assign a Pokemon to the class and allow easy changes (e.g., evolution)
    Update BattleTestView to reflect changes in the Pokemon class.

--- src/modules/Pokemon.ts
+++ src/modules/Pokemon.ts
@@ -1,47 +1,30 @@
-import PokemonType from "./PokemonType";
 import { PokemonData } from "./PokemonList";
 import { getTypeEffectiveness } from './manager/TypeManager'
 
 export default class Pokemon {
-    id: number;
-    name: string;
-    catchRate: number;
-    type: PokemonType[];
-    hitpoints: number;
-    attack: number;
-    specialAttack: number;
-    defense: number;
-    specialDefense: number;
-    speed: number;
+    pokemon: PokemonData
     level: number;
   
     constructor(pokemon: PokemonData, level: number)
     {
-        this.id = pokemon.id;
-        this.name = pokemon.name;
-        this.catchRate = pokemon.catchRate;
-        this.type = pokemon.type;
-        this.hitpoints = pokemon.hitpoints;
-        this.attack = pokemon.attack;
-        this.specialAttack = pokemon.specialAttack;
-        this.defense = pokemon.defense;
-        this.specialDefense = pokemon.specialDefense;
-        this.speed = pokemon.speed;
+        this.pokemon = pokemon;
         this.level = level;
     }
 
     public getInitialScore(): number
     {
-        return (this.hitpoints
-            + this.attack
-            + this.specialAttack
-            + this.defense
-            + this.specialDefense
-            + this.speed) * (this.level / 10);
+        return (this.pokemon.hitpoints
+            + this.pokemon.attack
+            + this.pokemon.specialAttack
+            + this.pokemon.defense
+            + this.pokemon.specialDefense
+            + this.pokemon.speed) * (this.level / 10);
     }
 
     public getScoreAgainstPokemon(opponent: Pokemon): number
     {
-        return this.getInitialScore() * getTypeEffectiveness(this.type,opponent.type);
+        return this.getInitialScore() * getTypeEffectiveness(this.pokemon.type,opponent.pokemon.type);
     }
+
+    public getName(): string { return this.pokemon.name }
 }
--- src/views/Test/BattleTestView.vue
+++ src/views/Test/BattleTestView.vue
@@ -35,33 +35,33 @@
 </script>
 
 <template>
-  <h1>Battle Test</h1>
-    <div class="container">
-        <div class="row">
-            <div class="col-6">
-                <h2>{{ player.name }}</h2>
-                <h3>Pokémon</h3>
-                <div v-if="combat.selectedPokemonPlayer" class="card shadow-sm">{{ combat.selectedPokemonPlayer.name }} ({{ combat.selectedPokemonPlayer.getInitialScore() }})</div>
-                <h3>Team</h3>
-                <div v-for="pokemon in player.pokemons" class="card shadow-sm" @click="selectPokemon(pokemon)">{{ pokemon.name }} ({{ pokemon.getInitialScore() }})</div>
-            </div>
-
-            <div class="col-6">
-                <h2>{{ bot.name }}</h2>
-                <h3>Pokémon</h3>
-                <div v-if="combat.selectedPokemonOpponent" class="card shadow-sm">{{ combat.selectedPokemonOpponent.name }} ({{ combat.selectedPokemonOpponent.getInitialScore() }})</div>
-                <h3>Team</h3>
-                <div v-for="pokemon in bot.pokemons" class="card shadow-sm">{{ pokemon.name }} ({{ pokemon.getInitialScore() }})</div>
-            </div>
-        </div>
-
-        <div class="row">
-            <h2>Historique Combat</h2>
-            <div v-for="div in combat.historical">
-                <div class="row">{{div}}</div>
-            </div>
-            <button v-if="combat.action == 'waiting'" type="button" class="btn btn-primary" @click="continueFight('fight')">Combat</button>
-            <button v-if="combat.action == 'continue'" type="button" class="btn btn-primary" @click="continueFight('continue')">Continuer</button>
-        </div>
-    </div>
-</template>
+    <h1>Battle Test</h1>
+      <div class="container">
+          <div class="row">
+              <div class="col-6">
+                  <h2>{{ player.name }}</h2>
+                  <h3>Pokémon</h3>
+                  <div v-if="combat.selectedPokemonPlayer" class="card shadow-sm">{{ combat.selectedPokemonPlayer.getName() }} ({{ combat.selectedPokemonPlayer.getInitialScore() }})</div>
+                  <h3>Team</h3>
+                  <div v-for="pokemon in player.pokemons" class="card shadow-sm" @click="selectPokemon(pokemon)">{{ pokemon.getName() }} ({{ pokemon.getInitialScore() }})</div>
+              </div>
+  
+              <div class="col-6">
+                  <h2>{{ bot.name }}</h2>
+                  <h3>Pokémon</h3>
+                  <div v-if="combat.selectedPokemonOpponent" class="card shadow-sm">{{ combat.selectedPokemonOpponent.getName() }} ({{ combat.selectedPokemonOpponent.getInitialScore() }})</div>
+                  <h3>Team</h3>
+                  <div v-for="pokemon in bot.pokemons" class="card shadow-sm">{{ pokemon.getName() }} ({{ pokemon.getInitialScore() }})</div>
+              </div>
+          </div>
+  
+          <div class="row">
+              <h2>Historique Combat</h2>
+              <div v-for="div in combat.historical">
+                  <div class="row">{{div}}</div>
+              </div>
+              <button v-if="combat.action == 'waiting'" type="button" class="btn btn-primary" @click="continueFight('fight')">Combat</button>
+              <button v-if="combat.action == 'continue'" type="button" class="btn btn-primary" @click="continueFight('continue')">Continuer</button>
+          </div>
+      </div>
+  </template>
