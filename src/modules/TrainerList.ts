ffeb32684e7109f5fc76a7f47d9c716fd873e644
Dekadence <deathfurious@hotmail.fr>
2023-05-09 20:11:10 +0200

    Integration of all Kanto Champions/Elites
    Changes:
    - Add champions/elites to the PlayerTrainerList.

--- src/modules/TrainerList.ts
+++ src/modules/TrainerList.ts
@@ -26,6 +26,127 @@ export const PlayerTrainerList = createChampionArray(
         ]
     },
     {
+        'id': 'lance',
+        'name': 'Lance',
+        'type': PlayerTrainerType.Elite,
+        'pokemonType': PokemonType.Dragon,
+        'pokemons': [
+            new Pokemon(getPokemonById(147), 10),
+            new Pokemon(getPokemonById(147), 10),
+            new Pokemon(getPokemonById(142), 10)
+        ]
+    },
+    {
+        'id': 'agatha',
+        'name': 'Agatha',
+        'type': PlayerTrainerType.Elite,
+        'pokemonType': PokemonType.Ghost,
+        'pokemons': [
+            new Pokemon(getPokemonById(23), 10),
+            new Pokemon(getPokemonById(92), 10),
+            new Pokemon(getPokemonById(92), 10)
+        ]
+    },
+    {
+        'id': 'bruno',
+        'name': 'Bruno',
+        'type': PlayerTrainerType.Elite,
+        'pokemonType': PokemonType.Fighting,
+        'pokemons': [
+            new Pokemon(getPokemonById(66), 10),
+            new Pokemon(getPokemonById(106), 10),
+            new Pokemon(getPokemonById(107), 10)
+        ]
+    },
+    {
+        'id': 'lorelei',
+        'name': 'Lorelei',
+        'type': PlayerTrainerType.Elite,
+        'pokemonType': PokemonType.Ice,
+        'pokemons': [
+            new Pokemon(getPokemonById(86), 10),
+            new Pokemon(getPokemonById(124), 10),
+            new Pokemon(getPokemonById(131), 10)
+        ]
+    },
+    {
+        'id': 'giovanni',
+        'name': 'Giovanni',
+        'type': PlayerTrainerType.GymLeader,
+        'pokemonType': PokemonType.Ground,
+        'pokemons': [
+            new Pokemon(getPokemonById(29), 10),
+            new Pokemon(getPokemonById(32), 10),
+            new Pokemon(getPokemonById(111), 10)
+        ]
+    },
+    {
+        'id': 'blaine',
+        'name': 'Blaine',
+        'type': PlayerTrainerType.GymLeader,
+        'pokemonType': PokemonType.Fire,
+        'pokemons': [
+            new Pokemon(getPokemonById(77), 10),
+            new Pokemon(getPokemonById(58), 10),
+            new Pokemon(getPokemonById(126), 10)
+        ]
+    },
+    {
+        'id': 'sabrina',
+        'name': 'Sabrina',
+        'type': PlayerTrainerType.GymLeader,
+        'pokemonType': PokemonType.Psychic,
+        'pokemons': [
+            new Pokemon(getPokemonById(63), 10),
+            new Pokemon(getPokemonById(63), 10),
+            new Pokemon(getPokemonById(122), 10)
+        ]
+    },
+    {
+        'id': 'koga',
+        'name': 'Koga',
+        'type': PlayerTrainerType.GymLeader,
+        'pokemonType': PokemonType.Psychic,
+        'pokemons': [
+            new Pokemon(getPokemonById(48), 10),
+            new Pokemon(getPokemonById(88), 10),
+            new Pokemon(getPokemonById(109), 10)
+        ]
+    },
+    {
+        'id': 'erika',
+        'name': 'Erika',
+        'type': PlayerTrainerType.GymLeader,
+        'pokemonType': PokemonType.Grass,
+        'pokemons': [
+            new Pokemon(getPokemonById(69), 10),
+            new Pokemon(getPokemonById(43), 10),
+            new Pokemon(getPokemonById(114), 10)
+        ]
+    },
+    {
+        'id': 'ltsurge',
+        'name': 'Lt. Surge',
+        'type': PlayerTrainerType.GymLeader,
+        'pokemonType': PokemonType.Electric,
+        'pokemons': [
+            new Pokemon(getPokemonById(25), 10),
+            new Pokemon(getPokemonById(26), 10),
+            new Pokemon(getPokemonById(81), 10)
+        ]
+    },
+    {
+        'id': 'misty',
+        'name': 'Misty',
+        'type': PlayerTrainerType.GymLeader,
+        'pokemonType': PokemonType.Water,
+        'pokemons': [
+            new Pokemon(getPokemonById(116), 10),
+            new Pokemon(getPokemonById(118), 10),
+            new Pokemon(getPokemonById(120), 10)
+        ]
+    },
+    {
         'id': 'brock',
         'name': 'Brock',
         'type': PlayerTrainerType.GymLeader,
@@ -35,7 +156,7 @@ export const PlayerTrainerList = createChampionArray(
             new Pokemon(getPokemonById(74), 10),
             new Pokemon(getPokemonById(95), 10)
         ]
-    }
+    },
 )
 
 export const BotTrainerList = createChampionArray(
